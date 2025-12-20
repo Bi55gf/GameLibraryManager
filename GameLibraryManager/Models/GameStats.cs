ba0be34b6c33e665d6b90553a3aeeba8f9fe96a9
Jadon Broad <bi55gf@student.sunderland.ac.uk>
2025-12-20 20:59:23 +0000

    Added Player and GameStats classes

---
+++ GameLibraryManager/Models/GameStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibraryManager.Models
+{
+    public class GameStats
+    {
+        public int HoursPlayed { get; set; }
+        public int HighScore { get; set; }
+    }
+}
---
+++ GameLibraryManager/Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibraryManager.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public GameStats Stats { get; set; }
+
+        public Player()
+        {
+            Stats = new GameStats();
+        }
+    }
+}
