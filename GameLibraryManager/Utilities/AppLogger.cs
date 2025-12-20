61901c36244117bee33e5da6cb7ff10f56853d32
Jadon Broad <bi55gf@student.sunderland.ac.uk>
2025-12-20 22:13:35 +0000

    added the code required for the AppLogger and all references, im recieving a error code but will amend at a later time.

---
+++ GameLibraryManager/Utilities/AppLogger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GameLibraryManager.Utilities
+{
+    // Singleton Logger Class
+    private sealed class AppLogger
+    {
+        private static AppLogger instance;
+        private static readonly object lockObj = new();
+        private const string LogFile = "actions.log";
+
+        private AppLogger() { }
+
+        // Singleton instance property
+        public static AppLogger Instance
+        {
+            get
+            {
+                lock (lockObj)
+                {
+                    return instance ??= new AppLogger();
+                }
+            }
+        }
+
+        // Log method to append messages to the log file
+        public void Log(string message)
+        {
+            File.AppendAllText(LogFile, $"{DateTime.Now} : {message}{Environment.NewLine}");
+        }
+    }
+}
