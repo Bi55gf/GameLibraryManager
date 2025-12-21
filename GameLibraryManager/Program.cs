053a3a93a97bdd38a8bbc9f003bc852048bae421
Jadon Broad <bi55gf@student.sunderland.ac.uk>
2025-12-21 19:39:29 +0000

    fixed another naming error

--- GameLibraryManager/Program.cs
+++ GameLibraryManager/Program.cs
@@ -68,7 +68,7 @@ class program
         var player = new Player
         {
             PlayerId = id,
-            Username = username,
+            UserName = username,
             Stats =
             {
                 HoursPlayed = hoursPlayed,
