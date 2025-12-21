e35067981e249c4370a7ba848d8d3070d1ce56e2
Jadon Broad <bi55gf@student.sunderland.ac.uk>
2025-12-21 17:55:53 +0000

    added the JSON storage for player data, still need to fix some errors with AppLogger to fully complete

---
+++ GameLibraryManager/Services/PlayerStorage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using GameLibraryManager.Models;
+using GameLibraryManager.Utilities;
+using System.Text.Json;
+
+namespace GameLibraryManager.Services
+{
+    public class PlayerStorage
+    {
+        private const string FileName = "players.json";
+
+        public void Save(List<Player> players)
+        {
+            try
+            {
+                var json = JsonSerializer.Serialize(players, new JsonSerializerOptions 
+                { 
+                    WriteIndented = true 
+                });
+
+                File.WriteAllText(FileName, json);
+                AppLogger.Instance.Log("Player data saved successfully.");
+            }
+            catch
+            {
+                AppLogger.Instance.Log("Error saving player data.");
+            }
+        }
+
+        public List<Player> Load()
+        {
+            try
+            {
+                if (!File.Exists(FileName))
+                    return new List<Player>();
+
+                var json = File.ReadAllText(FileName);
+                return JsonSerializer.Deserialize<List<Player>>(json) ?? new List<Player>();
+            }
+            catch
+            {
+                AppLogger.Instance.Log("Error loading player data.");
+                return new List<Player>();
+            }
+        }
+    }
+}
